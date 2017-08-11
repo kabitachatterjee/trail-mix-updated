9586ca2e6a11e94add9b923ec3048a4959034235
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-11 14:59:53 -0700

    added the login and signup pages with facebook authentication

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '331536807288588',
+		'clientSecret': 'ea8d1ef0de74a21b75cac6a2e2dfe31d',
+		'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+	}
+}
