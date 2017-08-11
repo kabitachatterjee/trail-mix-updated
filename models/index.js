2df9b75b6cc4195e648c1a39b8e5358211e969ac
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-08-11 13:34:43 -0700

    added user schema to the database

--- models/index.js
+++ models/index.js
@@ -3,3 +3,4 @@ mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/trail-mix");
 
 //require & export the trails model through index.js
 module.exports.Trail = require("./trail.js");
+module.exports.User = require("./user.js");
